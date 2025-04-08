adac1807fc5c3c6ac6bc49f43db0da87911124e3
lenguyen2022 <dlnguyen@gmail.com>
2025-04-07 21:17:58 -0400

    Add default routes, and PageNotFound routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,18 @@
 import { Routes } from '@angular/router';
 import {ParentComponent} from './parent/parent.component';
 import {ChildComponent} from './parent/child/child.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {FunComponent} from './fun/fun.component';
+import {AboutComponent} from './about/about.component';
 
 export const routes: Routes = [
-  {path:'parent', component: ParentComponent,children:[{path:'child', component:ChildComponent}]}
+  {path: '', redirectTo:'/parent', pathMatch: 'full'},
+  {path:'parent', component: ParentComponent,children:[{path:'child', component:ChildComponent}]},
+  {path:'about', component: AboutComponent},
+  { path: '**', component: PageNotFoundComponent },
+
+
+
+  //Default Route
+  //A route with path '' is commonly used as the "default" route, which is the route Angular will load when no other route is specified by the user.
 ];
