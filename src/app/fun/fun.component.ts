6d2f69cefe81e661630d7b2fb1d008dd5f73f067
Le Nguyen <dlnguyen@gmail.com>
2025-04-08 08:59:08 -0400

    add about, fun, and page not found components

---
+++ src/app/fun/fun.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fun',
+  imports: [],
+  templateUrl: './fun.component.html',
+  styleUrl: './fun.component.css'
+})
+export class FunComponent {
+
+}
